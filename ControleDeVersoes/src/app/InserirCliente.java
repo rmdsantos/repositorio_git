a3f178886c505c3b765e20a348f0ef91c88d09bc
rmdsantos <rafael_monteiro_rms@hotmail.com>
2013-11-20 12:01:30 -0200

    commit 6

--- ControleDeVersoes/src/app/InserirCliente.java
+++ ControleDeVersoes/src/app/InserirCliente.java
@@ -6,8 +6,8 @@ public class InserirCliente {
 	public static void main(String[] args) {
 
 		System.out.println("Modificacao JAVA");
-		System.out.println("Modificacao JAVA 2");
-		
+		System.out.println("Modificacao JAVA 2");		
+		System.out.println("Modificacao JAVA 3");
 	}
 
 }
