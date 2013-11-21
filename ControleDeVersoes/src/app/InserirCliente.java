7edcea872a2370c373896db32d8bb08696a5d529
rmdsantos <rafael_monteiro_rms@hotmail.com>
2013-11-21 21:12:10 -0200

    Commit 8 Inserindo Cliente

--- ControleDeVersoes/src/app/InserirCliente.java
+++ ControleDeVersoes/src/app/InserirCliente.java
@@ -1,14 +1,35 @@
 package app;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Cliente;
+
 public class InserirCliente {
 
 	
 	public static void main(String[] args) {
 
-		System.out.println("Modificacao JAVA");
-		System.out.println("Modificacao JAVA 2");		
-		System.out.println("Modificacao JAVA 3");
-		System.out.println("Modificaçao GIT HUB 7");
+		EntityManagerFactory factory =
+				Persistence.createEntityManagerFactory("qualidade");
+		EntityManager manager = factory.createEntityManager();
+		
+		manager.getTransaction().begin();
+		
+		Cliente c = new Cliente();
+		
+		c.setNome("Rafael Monteiro");
+		
+		c.setEmail("rafael@qualidadesoftware.com.br");
+		
+		manager.persist(c);
+		
+		manager.getTransaction().commit();
+		
+		manager.close();
+		factory.close();
+		
 	}
 
 }
